89c1eb4d8b224f0a57fe77962e4fb39fbacc704e
ljv29 <jumpswervelogan@gmail.com>
2020-05-09 09:19:50 -0700

    final commit, took out console.logs

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,35 +4,23 @@ const router = express.Router()
 
 
 router.get("/", function (req, res) {
-  console.log("get")
   burger.selectAll(function (data) {
-
-    // preapare for render in the handlebars
     res.render("index", { burgers: data })
   })
-
 })
 
 
 router.post("/api/burgers", function (req, res) {
-  console.log("POST: ", req.body)
-  //calling burger ORM to create a burger 
   burger.insertOne(["burger_name"], [req.body.name], function (data) {
-
     res.json(data)
   })
-
 })
 
 router.put("/api/burgers/:id", function (req, res) {
-  console.log("PUT: ", req.params)
-
   let condition = "id=" + req.params.id
   burger.updateOne({ devoured: true }, condition, function (data) {
     res.json(data)
-
   })
-
 })
 
 router.delete("/api/burgers/:id", function (req, res) {
